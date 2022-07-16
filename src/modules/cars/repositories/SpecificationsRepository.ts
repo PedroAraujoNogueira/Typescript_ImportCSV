b3cf541a4e5808025c6fea0a1aab1152d5369c42
PedroAraujo <pedro.nogueira@contazap.com>
2022-07-16 00:24:14 -0300

    Criando repositório e service de especificação

--- src/modules/cars/repositories/SpecificationsRepository.ts
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -1,11 +1,33 @@
+import { Specification } from "../model/Specification";
 import {
   ISpecificationsRepository,
   ICreateSpecificationDTO,
 } from "./ISpecificationsRepository";
 
 class SpecificationRepository implements ISpecificationsRepository {
-  create({ name, string }: ICreateSpecificationDTO): void {
-    throw new Error("Method not found");
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+    return specification;
   }
 }
 
